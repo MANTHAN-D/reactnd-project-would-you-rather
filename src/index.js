2a1257d8b5f40a9b175ea93839cb6459a9c2cfaf
madoshi <madoshi@ebay.com>
2018-11-27 06:50:01 -0800

    Store creation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { createStore } from 'redux'
 
 import App from './App'
 import './index.css'
+import reducers from './reducers/index'
+import middleware from './middleware/index'
+
+const store = createStore(reducers, middleware)
 
 ReactDom.render(<App />, document.getElementById('root'))
