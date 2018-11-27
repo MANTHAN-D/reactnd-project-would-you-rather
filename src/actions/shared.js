1759ee7afde2dc881dd43f087ea54184cef251f1
madoshi <madoshi@ebay.com>
2018-11-27 06:48:50 -0800

    Inital action creators

---
+++ src/actions/shared.js
@@ -0,0 +1,13 @@
+import { _getQuestions, _getUsers } from '../../_DATA'
+import { receiveQuestions } from './questions'
+import { receiveUsers } from './users'
+
+export const handleInitialData = () => {
+  _getQuestions().then(({ questions }) => {
+    dispatch(receiveQuestions(questions))
+  })
+
+  _getUsers().then(({ users }) => {
+    dispatch(receiveUsers(users))
+  })
+}
