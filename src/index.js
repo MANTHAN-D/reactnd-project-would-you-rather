93df6cae4e444fe27623d82e6f195e7dc8ec6cde
madoshi <madoshi@ebay.com>
2018-11-27 07:01:45 -0800

    Connected store with app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import App from './App'
 import './index.css'
@@ -9,4 +10,9 @@ import middleware from './middleware/index'
 
 const store = createStore(reducers, middleware)
 
-ReactDom.render(<App />, document.getElementById('root'))
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
