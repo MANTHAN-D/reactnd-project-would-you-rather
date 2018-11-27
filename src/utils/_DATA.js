8a50647f9dc2eb814df1277549db65ad586ea5c1
madoshi <madoshi@ebay.com>
2018-11-27 06:48:04 -0800

    Refactored and added missing information

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: ,
+    avatarURL: 'https://github.com/MANTHAN-D/reactnd-project-would-you-rather/blob/master/public/avatars/sarah.png?raw=true',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: ,
+    avatarURL: 'https://github.com/MANTHAN-D/reactnd-project-would-you-rather/blob/master/public/avatars/tyler.png?raw=true',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: ,
+    avatarURL: 'https://github.com/MANTHAN-D/reactnd-project-would-you-rather/blob/master/public/avatars/john.png?raw=true',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
@@ -35,7 +35,7 @@ let users = {
   monty: {
     id: 'monty',
     name: 'Monty D',
-    avatarURL: ,
+    avatarURL: 'https://github.com/MANTHAN-D/reactnd-project-would-you-rather/blob/master/public/avatars/monty.png?raw=true',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
